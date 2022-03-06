08f21580893b11d81e3771145c350372fac154fb
Jayshree Kiran <93006422+jjaysh@users.noreply.github.com>
2022-03-06 19:30:46 +0530

    Question 2

---
+++ Feb 26/Qsts2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Qsts2 {
+    public static void main(String[] args){
+        int[] numsOne = {9,4,9,8,4};
+        int[] numsTwo = {4,9,5};
+        int lengthOne = numsOne.length;
+        int lengthTwo = numsTwo.length;
+        int i = 0,j = 0,k = 0;
+        Arrays.sort(numsOne);//sorting array in ascending
+        Arrays.sort(numsTwo);
+
+        while(i < lengthOne  && j < lengthTwo){
+            if(numsOne[i] < numsTwo[j]){
+                i++;
+            }else if(numsOne[i] > numsTwo[j]){
+                j++;
+            }else {
+                numsOne[k+1]=numsOne[i++];//in k value of intersection b/w two number will be save
+                j++;
+                System.out.println("intersection of array is "+numsOne[i]+" "+numsTwo[k]);
+            }
+
+        }
+    }
+}
