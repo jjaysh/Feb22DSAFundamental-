9de8e0a9a39d8c9599a76b8ec4c29c16bad5806c
Jayshree Kiran <93006422+jjaysh@users.noreply.github.com>
2022-04-03 21:18:31 +0530

    WEEK5 - ASSIGN19

---
+++ median.java
@@ -0,0 +1,21 @@
+package march.week5.assignment19;
+
+import java.util.*;
+
+class median {
+
+    public static double findMedian(int a[], int n) {
+        Arrays.sort(a);
+
+        if (n % 2 != 0)
+            return (double) a[n / 2];
+
+        return (double) (a[(n - 1) / 2] + a[n / 2]) / 2.0;
+    }
+
+    public static void main(String args[]) {
+        int a[] = { 1, 3, 4, 2, 7, 5, 8, 6, 4, 9 };
+        int n = a.length;
+        System.out.println("Median = " + findMedian(a, n));
+    }
+}
